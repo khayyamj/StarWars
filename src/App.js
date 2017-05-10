76a655578352e70e37e537fa6f709edfebeebf96
Jolt Testing <test@intuiplan.com>
2017-05-10 15:09:11 -0600

    limit loading to 10 profiles at a time with click

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ class App extends Component {
       searchTerm: ''
     }
     this.renderPeople = this.renderPeople.bind(this);
+    this.limitLoading = this.limitLoading.bind(this);
   }
   componentDidMount () {
     axios('http://localhost:3008/people')
@@ -38,7 +39,13 @@ class App extends Component {
     })
   }
   renderPeople () {
-    return this.state.people.map(person => {
+
+      return this.state.people
+      .filter(person => {
+        console.log('filter: ', `${person.name}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
+        return (`${person.name}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
+      })
+      .map(person => {
       let home = this.state.home
       let planetIndex = person.homeworld;
       let planet = home.filter( (planet) => {
@@ -47,13 +54,17 @@ class App extends Component {
       let homePlanet = planet[0].name;
       return (
         <div>
-          <Card home={homePlanet} person={person} />
+          <Card home={homePlanet} person={person} key={person.id}/>
         </div>
       )
     })
   }
   handleSearchChange (event) {
     this.setState({ searchTerm: event.target.value })
+  }
+  limitLoading (lower, upper) {
+    axios(`http://localhost:3008/people?_start=${lower}&_end=${upper}`)
+    .then(response => this.setState({ people: response.data }))
   }
   render() {
     return (
@@ -63,7 +74,39 @@ class App extends Component {
           <span className='interview-text'>The Interview</span>
           <img src={wars} alt="wars-logo" />
         </div>
-        <SearchBar onChange={this.handleSearchChange} value={this.state.searchTerm}/>
+        <div>
+          <h3>
+            <a onClick={() => this.limitLoading(0, 9)}>1 - 10</a>
+          </h3>
+          <h3>
+            <a onClick={() => this.limitLoading(10, 19)}>11 - 20</a>
+          </h3>
+          <h3>
+            <a onClick={() => this.limitLoading(20, 29)}>21 - 30</a>
+          </h3>
+          <h3>
+            <a onClick={() => this.limitLoading(30, 39)}>31 - 40</a>
+          </h3>
+          <h3>
+            <a onClick={() => this.limitLoading(40, 49)}>41 -50</a>
+          </h3>
+          <h3>
+            <a onClick={() => this.limitLoading(50, 59)}>51 - 60</a>
+          </h3>
+          <h3>
+            <a onClick={() => this.limitLoading(60, 69)}>61 - 70</a>
+          </h3>
+          <h3>
+            <a onClick={() => this.limitLoading(70, 79)}>71 - 80</a>
+          </h3>
+          <h3>
+            <a onClick={() => this.limitLoading(80, 82)}>81 - 83</a>
+          </h3>
+        </div>
+        <SearchBar
+          onChange={this.handleSearchChange}
+          value={this.state.searchTerm}/>
+          {this.state.searchTerm}
         {this.renderPeople()}
       </div>
     );
